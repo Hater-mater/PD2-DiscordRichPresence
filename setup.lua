a6a55d4c5d8d0062331d242d579fb6a186fb172b
Hater-mater <swetlaniki@gmail.com>
2026-04-09 14:46:23 +0300

    Added missing icon when you in VC
    Bro I made a new app for this wtf wrong with Discord

--- setup.lua
+++ setup.lua
@@ -1,3 +1,8 @@
 Hooks:PreHook(Setup, "init_managers", "post_hook_init_managers_drp", function(self, managers)
-    Discord:init("1484159961536921720")
+    Discord:init("1491748575200411748")
 end)
+-- Old ID is 1484159961536921720
+--[[
+You fucking kidding me? To add image for app when you in VC I need remake an app because adding an image to existing app do not work because it was inited previously????
+Discord is a menace holy shit
+]]--
